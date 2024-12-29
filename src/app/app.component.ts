7052732d00d292d8715785d0eddcc45fa844ef63
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-29 16:24:25 +0100

    Feat : import survey list componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SurveysListComponent} from './features/survey/components/surveys-list/surveys-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SurveysListComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
