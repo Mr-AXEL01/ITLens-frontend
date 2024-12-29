c93e6312bae525e137b763cdefc57ff0b92d97c0
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-29 20:37:10 +0100

    set the admin layout header component

---
+++ src/app/core/layout/admin-layout/components/admin-layout-header/admin-layout-header.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layout-header',
+  standalone: true,
+  templateUrl: './admin-layout-header.component.html',
+})
+export class AdminLayoutComponent {
+
+}
