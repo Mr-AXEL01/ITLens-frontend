12eb6bd22642e0cffd80be444c73e5bbdb8f52aa
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-29 22:20:20 +0100

    set the template for the admin layout component
     by calling the admin layout footer && header and set the router outlet in the main section to present childrens

--- src/app/core/layout/admin-layout/admin-layout.component.ts
+++ src/app/core/layout/admin-layout/admin-layout.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {AdminLayoutHeaderComponent} from '../../components/admin-layout-header/admin-layout-header.component';
+import {AdminLayoutFooterComponent} from '../../components/admin-layout-footer/admin-layout-footer.component';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    AdminLayoutHeaderComponent,
+    AdminLayoutFooterComponent,
+  ],
   standalone: true,
-  templateUrl: './admin-layout.component.html',
-  styleUrl: './admin-layout.component.css'
+  template: `
+    <app-admin-layout-header></app-admin-layout-header>
+    <main class="min-h-[29.4rem] bg-gray-100 p-6">
+      <router-outlet></router-outlet>
+    </main>
+    <app-admin-layout-footer></app-admin-layout-footer>
+  `
 })
 export class AdminLayoutComponent {
 
