8b450de6643ae0f05da128a766c74eb442fda2cc
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-29 22:11:48 +0100

    import the router outlet
    style code by removing unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {SurveysListComponent} from './features/survey/components/surveys-list/surveys-list.component';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SurveysListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
