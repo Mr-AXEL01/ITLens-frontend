1c8863a747cc917ae4809c0b1f70bef3585c0219
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-29 16:25:58 +0100

    implement OnInit, and iject the survey service

--- src/app/features/survey/components/surveys-list/surveys-list.component.ts
+++ src/app/features/survey/components/surveys-list/surveys-list.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {Survey} from '../../models/survey.model';
+import {SurveyService} from '../../services/survey.service';
 
 @Component({
   selector: 'app-surveys-list',
   imports: [],
+  standalone: true,
   templateUrl: './surveys-list.component.html',
   styleUrl: './surveys-list.component.css'
 })
-export class SurveysListComponent {
+export class SurveysListComponent implements OnInit{
+  surveys!: Survey[];
 
+  surveyService = inject(SurveyService);
+
+  ngOnInit() {
+    this.surveyService.getSurveys().subscribe({
+      next: (data) => {
+        this.surveys = data;
+        console.log(this.surveys);
+      }
+    })
+  }
 }
