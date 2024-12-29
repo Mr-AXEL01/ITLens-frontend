ebdf2d2a436ea71ca99da2836f00f65caf650078
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-29 22:10:40 +0100

    handle route
    Fix routing configuration to properly handle AdminLayoutComponent and child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import {AdminLayoutComponent} from './core/layout/admin-layout/admin-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: 'surveys',
+        loadComponent: () =>
+          import('./features/survey/pages/surveys-list/surveys-list.component')
+            .then(c => c.SurveysListComponent)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
