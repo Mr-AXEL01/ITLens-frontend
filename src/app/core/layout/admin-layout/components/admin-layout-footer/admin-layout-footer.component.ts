52b59a230012d4ffe10e3a5b1a0866e7ec818282
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-29 20:34:28 +0100

    add components for the admin layout (admin-layout- {footer, header})

---
+++ src/app/core/layout/admin-layout/components/admin-layout-footer/admin-layout-footer.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layout-footer',
+  standalone: true,
+  templateUrl: './admin-layout-footer.component.html',
+})
+export class AdminLayoutComponent {
+
+}
