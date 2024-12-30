52e4005097f5d4687295073f2521f7c842a7c0fe
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-30 17:07:15 +0100

    Feat : add a modal form
    set a modal form to handle the create and the update of the survey

---
+++ src/app/features/survey/components/survey-item/survey-item.component.ts
@@ -0,0 +1,32 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Survey} from '../../models/survey.model';
+import {NgForOf} from '@angular/common';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-survey-item',
+  standalone: true,
+  imports: [
+    NgForOf,
+    RouterLink
+  ],
+  templateUrl: './survey-item.component.html'
+})
+export class SurveyItemComponent {
+  @Input() survey!: Survey;
+  @Output() updateSurvey = new EventEmitter<Survey>();
+  @Output() deleteSurvey = new EventEmitter<string>();
+
+  onUpdate(): void {
+    const updatedSurvey = { ...this.survey };
+    // updatedSurvey.title = prompt('Enter new title:', this.survey.title) || this.survey.title;
+    // updatedSurvey.description = prompt('Enter new description:', this.survey.description) || this.survey.description;
+    this.updateSurvey.emit(updatedSurvey);
+  }
+
+  onDelete(): void {
+    if (confirm(`Are you sure you want to delete the survey "${this.survey.title}"?`)) {
+      this.deleteSurvey.emit(this.survey.id);
+    }
+  }
+}
