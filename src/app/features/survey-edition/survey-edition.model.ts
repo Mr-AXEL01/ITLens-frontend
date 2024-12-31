f47d38f49d099fede9c14e1656e999046041ad4e
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-31 14:23:20 +0100

    Feat : add the survey-edition-request
    set the interface SurveyEditionRequest to use for the creation and the update of the surveyedition.

--- src/app/features/survey-edition/survey-edition.model.ts
+++ src/app/features/survey-edition/survey-edition.model.ts
@@ -4,3 +4,10 @@ export interface SurveyEdition {
   startDate: Date;
   year: number;
 }
+
+export interface SurveyEditionRequest {
+  creationDate: Date;
+  startDate: Date;
+  year: number;
+  surveyId: string;
+}
