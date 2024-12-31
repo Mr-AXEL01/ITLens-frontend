94644927bf258d955fd5819501116e3b16013abe
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-12-31 14:37:30 +0100

    add new get method
    set a new methid to get all the surveyEditions

--- src/app/features/survey-edition/survey-edition.service.ts
+++ src/app/features/survey-edition/survey-edition.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {API_URL} from '../../envirenment';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {SurveyEdition} from './survey-edition.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SurveyEditionService {
+  private readonly path: string = API_URL + "/survey_editions";
 
-  constructor() { }
+  constructor(private readonly http: HttpClient) { }
+
+  public getAll(): Observable<SurveyEdition[]> {
+    return this.http.get<SurveyEdition[]>(this.path);
+  }
 }
