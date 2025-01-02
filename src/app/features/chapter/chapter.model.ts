c36cf6fec9cd22a140e775d8da3bb41a187d6312
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-02 11:20:22 +0100

    add interfaces for question and answer
    set the interface response for the question and an enum for the question type + an embeddedAnswer

--- src/app/features/chapter/chapter.model.ts
+++ src/app/features/chapter/chapter.model.ts
@@ -22,5 +22,21 @@ export interface EmbeddedChapter {
 }
 
 export interface Question {
+  id: string;
+  text: string;
+  questionType: QuestionType;
+  answerCount: number;
+  chapter: EmbeddedChapter;
+  answers: EmbeddedAnswer[];
+}
 
+export enum QuestionType {
+  SINGLE_CHOICE = "SINGLE_CHOICE",
+  MULTIPLE_CHOICE = "MULTIPLE_CHOICE"
+}
+
+export interface EmbeddedAnswer {
+  id: string;
+  text: string;
+  selectionCount: number;
 }
