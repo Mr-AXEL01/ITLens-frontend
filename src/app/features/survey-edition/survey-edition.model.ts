cdc2a705bf7b2bfff376f32d5b7da7b950b1281a
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-02 10:08:27 +0100

    update surveyedition interface
    change the surveyEdition interface to have the object of the survey that have it + list of chapters that it's inculde.

--- src/app/features/survey-edition/survey-edition.model.ts
+++ src/app/features/survey-edition/survey-edition.model.ts
@@ -1,8 +1,13 @@
+import {Survey} from '../survey/models/survey.model';
+import {EmbeddedChapter} from '../chapter/chapter.model';
+
 export interface SurveyEdition {
   id: string;
   creationDate: Date;
   startDate: Date;
   year: number;
+  survey: Survey;
+  chapters: EmbeddedChapter[];
 }
 
 export interface SurveyEditionRequest {
