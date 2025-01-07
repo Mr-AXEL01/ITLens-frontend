d5b71f24d23f9a1d9f4f7b950504dc33147b7f9f
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-07 22:37:37 +0100

    change selectChapter() and loadSurveyEdition()
    update the method loadSurveyEdition() to get the survey-edition by ID ; and update selectChapter() to get questions by subChapter ID.

--- src/app/features/survey-edition/pages/survey-detail/survey-detail.component.ts
+++ src/app/features/survey-edition/pages/survey-detail/survey-detail.component.ts
@@ -2,11 +2,12 @@ import {Component, OnInit} from '@angular/core';
 import {SurveyEdition} from '../../survey-edition.model';
 import {SurveyEditionService} from '../../survey-edition.service';
 import {ActivatedRoute} from '@angular/router';
-import {Chapter, EmbeddedChapter} from '../../../chapter/chapter.model';
+import {Chapter} from '../../../chapter/chapter.model';
 import {NgClass, NgForOf, NgIf} from '@angular/common';
 import {ChapterService} from '../../../chapter/chapter.service';
 import {forkJoin} from "rxjs";
-import {Question} from '../../../question/question.model';
+import {Question, QuestionType} from '../../../question/question.model';
+import {QuestionService} from '../../../question/question.service';
 
 @Component({
   selector: 'app-survey-detail',
@@ -24,10 +25,13 @@ export class SurveyDetailComponent implements OnInit {
   rootChapters: Chapter[] = [];
   selectedChapter?: Chapter;
   selectedQuestion?: Question;
+  questions?: Question[] = [];
+  isLoading = false;
 
   constructor(
     private readonly surveyEditionService: SurveyEditionService,
     private readonly chapterService: ChapterService,
+    private readonly questionService: QuestionService,
     private readonly route: ActivatedRoute
   ) {}
 
@@ -40,30 +44,30 @@ export class SurveyDetailComponent implements OnInit {
     this.surveyEditionService.getById(this.surveyEditionId).subscribe({
       next: (data) => {
         this.surveyEdition = data;
-        this.prepareRootChapters(data.chapters);
+        this.rootChapters = this.surveyEdition.chapters;
       },
-      error: (err) => console.error('Failed to load survey-edition ', err),
+      error: (err) => console.error('Failed to load survey edition', err),
     });
   }
 
-  private prepareRootChapters(embeddedChapters: EmbeddedChapter[]): void {
-    const chapterRequests = embeddedChapters.map((embeddedChapter) =>
-      this.chapterService.getById(embeddedChapter.id)
-    );
-
-    forkJoin(chapterRequests).subscribe({
-      next: (fullChapters) => {
-        this.rootChapters = fullChapters.filter(
-          (chapter) => !chapter.parentChapter
-        );
-      },
-      error: (err) => console.error('Failed to load chapters', err),
-    });
-  }
 
   selectChapter(chapter: Chapter): void {
+    this.selectedQuestion = undefined;
+    this.isLoading = true;
+    this.questionService.getByChapterId(chapter.id).subscribe({
+      next: (data) => {
+        this.questions = data;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Failed to fetch questions for chapter', err);
+        this.questions = [];
+        this.isLoading = false;
+      },
+    });
     this.selectedChapter = chapter;
   }
+
 
   selectQuestion(question: Question): void {
     this.selectedQuestion = question;
