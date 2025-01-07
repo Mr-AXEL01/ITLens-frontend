08f95ad91678754d5509803e14850ec2a275e2fc
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2025-01-07 22:12:25 +0100

    Docs : update attribute dataType
    set the answers to be array of the interface Answer && the chapter to be object of the interface Chapter.

--- src/app/features/question/question.model.ts
+++ src/app/features/question/question.model.ts
@@ -1,12 +1,12 @@
-import {EmbeddedChapter} from '../chapter/chapter.model';
+import {Chapter} from '../chapter/chapter.model';
 
 export interface Question {
   id: string;
   text: string;
   questionType: QuestionType;
   answerCount: number;
-  chapter: EmbeddedChapter;
-  answers: EmbeddedAnswer[];
+  chapter: Chapter;
+  answers: Answer[];
 }
 
 export enum QuestionType {
@@ -14,12 +14,6 @@ export enum QuestionType {
   MULTIPLE_CHOICE = "MULTIPLE_CHOICE"
 }
 
-export interface EmbeddedQuestion {
-  id: string;
-  text: string;
-  questionType: QuestionType;
-  answerCount: number;
-}
 
 export interface QuestionRequest {
   text: string;
@@ -32,8 +26,9 @@ export interface AnswerRequest {
   text: string;
 }
 
-export interface EmbeddedAnswer {
+export interface Answer {
   id: string;
   text: string;
   selectionCount: number;
+  question: Question;
 }
